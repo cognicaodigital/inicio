1c312def4e02e4afa5188128ceaee762b8da5787
Cognicao Digital User <contato@cognicaodigital.com.br>
2026-07-21 14:43:22 -0300

    revert: restore global site Footer on /apresentacao page

--- src/App.jsx
+++ src/App.jsx
@@ -51,8 +51,7 @@ const PageWrapper = ({ children }) => {
 function AppContent() {
   const location = useLocation();
   const isPreview = location.pathname === '/cases/preview';
-  const isPresentation = location.pathname === '/apresentacao';
-
+  
   return (
     <div className="flex flex-col min-h-screen bg-cd-navy overflow-x-hidden">
       {!isPreview && <Navbar />}
@@ -81,7 +80,7 @@ function AppContent() {
           </PageWrapper>
         </Suspense>
       </main>
-      {!isPreview && !isPresentation && <Footer />}
+      {!isPreview && <Footer />}
       {!isPreview && <CookieBanner />}
     </div>
   );
